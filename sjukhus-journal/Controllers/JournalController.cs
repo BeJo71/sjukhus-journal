15ab1aaae81ecb75ee8ed2f3f34cf9cd4fdb0a6c
BeJo71 <beeajohansson@hotmail.se>
2025-03-02 18:36:09 +0100

    Update JournalController.cs
    La till delete funktion, kanske är onödigt än så länge?

--- sjukhus-journal/Controllers/JournalController.cs
+++ sjukhus-journal/Controllers/JournalController.cs
@@ -18,5 +18,37 @@ public class JournalController : ControllerBase
         public ActionResult<IEnumerable<Journal>> GetJournals()
         {
             return _context.Journaler.ToList();
+        } 
+
+         [HttpDelete("{id}")] //radera en journal, kanske onödigt att ta bort en hel journal? 
+        public IActionResult DeleteJournal(int id)
+        {
+        var journal = _context.Journaler.Find(id);
+
+        if (journal == null)
+        {
+        return NotFound(); 
         }
+
+        _context.Journaler.Remove(journal);
+        _context.SaveChanges(); 
+
+        return NoContent(); 
+        }
+ 
+    [HttpPut("clear-note/{id}")] // radera en anteckning
+    public IActionResult ClearNote(int id)
+    {
+        var journal = _context.Journaler.Find(id);
+
+        if (journal == null)
+        {
+            return NotFound(); 
+        }
+
+        journal.Anteckning = ""; 
+        _context.SaveChanges();  
+
+        return NoContent(); 
+    }
 }
